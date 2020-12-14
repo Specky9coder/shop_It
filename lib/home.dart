0337fda454da999b5ee34e63f30e32d919336081
keyur bhadani <specky9coder@gmail.com>
2020-12-14 12:54:50 +0530

    Initial commit

---
+++ lib/home.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Shop It'),
+      ),
+
+      body: Center(child: const Text('Let\'s Shop!'),),
+    );
+  }
+}
